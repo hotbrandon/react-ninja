ce0f0c1f44919d321a0725b278999be838be17a7
hotbrandon <hotbrandon@hotmail.com>
2022-12-07 10:15:05 +0800

    add blog data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
+import {useState } from 'react'
+import BlogList from './BlogList';
+
 const Home = () => {
+  const [blogs, setblogs] = useState([
+    { id:1, title: "python 101", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", author: "brandon"},
+    { id:2, title: "python 102", body: "Aspernatur alias, fugiat rerum debitis quos, quasi ducimus minima magnam quaerat laudantium quibusdam ullam.", author: "bob"},
+    { id:3, title: "python 103", body: "Neque eveniet, illo quo iusto dignissimos dolor dolorum.", author: "jack"},
+  ])
+
   return (
     <div className="home">
-      <h2>homepage</h2>
+      <BlogList blogs={blogs}/>
+      
     </div>
   );
 };
